6771a1051844cd980f41ddd7fcd72e7cb0011ce3
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-19 19:33:18 +0100

    Modify routes.js to add additional routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const a = { name: "Bob", age: 26, greet: "Hi Bob" };
+  res.json(a);
+});
+
+router.get("/hello", (req, res) => {
+  res.render("hello");
 });
 
 module.exports = router;
