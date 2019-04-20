18258b2d3d665247150a46a6a1d840e7110df53b
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-20 13:35:13 +0100

    Import catchErrors to routes from errorHandlers.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
 router.get("/", storeController.homepage);
