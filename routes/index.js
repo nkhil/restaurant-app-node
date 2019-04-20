66e63b7db241530220f97d08e693650945780f9e
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-20 13:35:50 +0100

    Use catchErrors on POST /add route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ const { catchErrors } = require("../handlers/errorHandlers");
 // Do work here
 router.get("/", storeController.homepage);
 router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
+
 module.exports = router;
