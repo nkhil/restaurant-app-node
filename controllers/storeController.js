c443bc12c4b9e3b698657911b2c5db4968f5f5ad
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-21 17:56:01 +0100

    Modify storeController.js to add logic

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype is not allowed" }, false);
+    }
+  }
+};
 
 exports.homepage = (req, res) => {
   res.render("homepage");
@@ -9,12 +24,65 @@ exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
 
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+  // Check if there's no new file to resize
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once the photo's been written to the file system, call next
+  next();
+};
+
 exports.createStore = async (req, res) => {
   const store = await new Store(req.body).save();
   req.flash("success", `Successfully created ${store.name}`);
-  res.redirect(`/store/${store.slug}`);
+  res.redirect(`/stores/${store.slug}`);
 };
 
-exports.getStores = (req, res) => {
-  res.render("stores", { title: "Stores" });
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  console.log(store);
+  // 2. Render the edit form for the user to update the store
+  console.log("StoReee", store);
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = "Point";
+  // find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  console.log(store);
+  req.flash("success", `Successfully updated <strong>${store.name}</strong>.`);
+  res.redirect(`/stores/${store.slug}`);
+  // Redriect them the store and tell them it worked
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  // req.params.slug
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render("store", { title: `${store.name}`, store });
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const stores = await Store.getTagsList();
+  res.json(stores);
 };
