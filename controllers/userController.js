d0927ed41902f938c012447a5b872d3a3b3b97c0
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-24 14:06:21 +0100

    Add registerForm to useController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose");
 exports.loginForm = (req, res) => {
   res.render("login", { title: "Login" });
 };
+
+exports.registerForm = (req, res) => {
+  res.render("register", { title: "Register" });
+};
